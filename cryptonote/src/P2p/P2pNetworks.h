01ca740eb558f438e258869daba4050989eedf2b
neptuniumteam <mavrinevan@gmail.com>
2016-03-08 21:55:24 -0500

    v0.1.0.1

---
+++ cryptonote/src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x26, 0x9a, 0x83, 0x33, 0x81, 0x22, 0x5c, 0x1c, 0x92, 0x62, 0xa2, 0xa0, 0x78, 0xe3, 0x73, 0xc5 } };
+}
